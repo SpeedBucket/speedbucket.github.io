270238a6d6b33cba027f016ebdb9f6c4df101420
Preston Jones <psmak4@gmail.com>
2016-10-19 15:29:11 -0600

    Went back to VS compiling for Scss and JS

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
 	$('.header__nav-toggle').click(function (e) {
 		e.preventDefault();
 		$(this).find('span').toggleClass('fa-bars').toggleClass('fa-close');
-		$('body').toggleClass('body--toggled');
 		$('.nav').toggleClass('nav--toggled');
 	});
 });
