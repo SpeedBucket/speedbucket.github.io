455693bceb1009e7ff11cd5abcf2f9061ff19b4f
Preston Jones <psmak4@gmail.com>
2016-10-19 11:00:49 -0600

    Redesigned layout and moved to using Jekyll templates.

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿$(function () {
+	$('.header__nav-toggle').click(function (e) {
+		e.preventDefault();
+		$(this).find('span').toggleClass('fa-bars').toggleClass('fa-close');
+		$('body').toggleClass('body--toggled');
+		$('.nav').toggleClass('nav--toggled');
+	});
+});
