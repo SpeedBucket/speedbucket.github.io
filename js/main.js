efa2925746145716735e7dd7c3a202b44dd71f84
psmak4 <psmak4@gmail.com>
2016-11-05 16:30:00 -0600

    Added multiple pages and content along with styles for content.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 ﻿$(function () {
 	$('.header__nav-toggle').click(function (e) {
 		e.preventDefault();
-		$(this).find('span').toggleClass('fa-bars').toggleClass('fa-close');
-		$('.nav').toggleClass('nav--toggled');
+		$('body').css('overflow', 'hidden');
+		$('.nav').fadeIn('fast');
+	});
+
+	$('.nav__toggle').click(function (e) {
+		e.preventDefault();
+		$('.nav').fadeOut('fast');
+		$('body').css('overflow', 'visible');
 	});
 });
